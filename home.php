6149d74a283f1c85a1907e4138d893ed1523f4eb
simon-bee <simonbee1303@gmail.com>
2019-05-28 16:20:50 +0200

    affichage et pagination++

--- home.php
+++ home.php
@@ -2,20 +2,8 @@
 require_once 'vendor/autoload.php';
 $pdo = new PDO('mysql:dbname=blog;host=172.17.0.2;charset=UTF8', 'userblog', 'blogpwd');
 
-$req = "SELECT COUNT(*) FROM post";
-$step = $pdo->prepare($req);
-$step->execute();
-$count = $step->fetch();
-$total = $count[0]/10;
-
-if($_GET['page'] > $total || !ctype_digit($_GET['page'])){
-    $message = 'Page introuvable !';
-}else{
-    $message = '';
-    unset($message);
-}
-
-if (isset($_GET['page']) && $_GET['page'] <= $total && ctype_digit($_GET['page'])) {
+$count = $pdo->query("SELECT COUNT(id) FROM post")->fetch()[0]/10;
+if (isset($_GET['page']) && $_GET['page'] <= $count && ctype_digit($_GET['page'])) {
     $get = $_GET['page'] - 1;
     $sql = "SELECT * FROM post LIMIT 10 OFFSET ".$get."0";
 }else{
@@ -38,11 +26,10 @@ $result2 = $state->fetchAll();
     <header class="">
         <h1>Bienvenue</h1>
     </header>
-    <div class="alert"><?= $message ?></div>
     <section class="articles">
 <?php foreach($result2 as $result): ?>
         <article>
-            <h2><?= $result['id']." | ".$result['name']; ?></h2>
+            <h2><span><?= $result['id']." |</span> ".$result['name']; ?></h2>
             <p ><?= $result['content']; ?></p>
             <p><?= $result['created_at']; ?></p>
         </article>
@@ -51,9 +38,9 @@ $result2 = $state->fetchAll();
     <section class="pagination">
         <p class="pages">
             <a href="/">1</a>
-<?php for ($i=2; $i <= ($count[0]/10); $i++):?>
+            <?php for ($i=2; $i <= $count; $i++):?>
             <a href="/?page=<?= $i ?>"><?= $i ?></a>
-<?php endfor; ?>
+            <?php endfor; ?>
         </p>
     </section>
     <footer>
